bfb5a0bef378c68ff73878d9a191da0d561651a7
imduchuyyy <duchuy.124dk@gmail.com>
2026-01-28 14:27:20 +0700

    feat(state processor): draft viction processor

---
+++ core/viction_state_processor.go
@@ -0,0 +1,13 @@
+package core
+
+func IsEmptyTransaction() bool {
+	return true
+}
+
+func ApplyEmptyTransaction() (interface{}, error) {
+	return nil, nil
+}
+
+func ApplySignTransaction() (interface{}, error) {
+	return nil, nil
+}
